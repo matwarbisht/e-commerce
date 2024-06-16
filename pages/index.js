77dd1d04d812a4240515418887973da74662fdf9
matwar bisht <matwarbisht@gmail.com>
2024-06-16 13:30:39 +0530

    initial commit - scaffold nextjs tailwing and add google login using next-auth

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Home() {
+  const { data: session } = useSession()
+  if(!session) {
+    return (
+      <div
+        className={`bg-blue-900 w-screen h-screen flex items-center`}
+      >
+        <div className="text-center w-full">
+          <button className="bg-white p-2 px-4 rounded-lg" onClick={() => signIn('google')} >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <div>Logged In as {session.user.email}</div>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  )
+}
